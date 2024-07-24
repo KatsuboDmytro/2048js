a83bcdbf8d71a9b7a1f610163da6c245286bbefe
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-07-24 14:48:54 +0300

    update new tile creation

--- src/modules/Tile.class.js
+++ src/modules/Tile.class.js
@@ -23,15 +23,19 @@ export class Tile {
   setValue(value) {
     this.value = value;
     this.tileElement.textContent = value;
-    this.tileElement.style.setProperty("--bg-color", `${COLORS[value]}`);
-    this.tileElement.style.setProperty("--text-color", `${value >= 8 ? '#f9f6f2' : '#776e65'}`);
+    this.tileElement.style.setProperty('--bg-color', `${COLORS[value]}`);
+
+    this.tileElement.style.setProperty(
+      '--text-color',
+      `${value >= 8 ? '#f9f6f2' : '#776e65'}`,
+    );
   }
 
   setXY(x, y) {
     this.x = x;
     this.y = y;
-    this.tileElement.style.setProperty("--x", x);
-    this.tileElement.style.setProperty("--y", y);
+    this.tileElement.style.setProperty('--x', x);
+    this.tileElement.style.setProperty('--y', y);
   }
 
   removeFromDOM() {
@@ -39,16 +43,18 @@ export class Tile {
   }
 
   waitForTransitionEnd() {
-    return new Promise(resolve => {
-      this.tileElement.addEventListener(
-        "transitionend", resolve, { once: true });
+    return new Promise((resolve) => {
+      this.tileElement.addEventListener('transitionend', resolve, {
+        once: true,
+      });
     });
   }
 
   waitForAnimationEnd() {
-    return new Promise(resolve => {
-      this.tileElement.addEventListener(
-        "animationend", resolve, { once: true });
+    return new Promise((resolve) => {
+      this.tileElement.addEventListener('animationend', resolve, {
+        once: true,
+      });
     });
   }
 }
